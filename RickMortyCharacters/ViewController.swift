c963b93d9e1a3e39d1b45d33650b9a9bade80998
Victor Soe Ko <21376679+seonar22@users.noreply.github.com>
2023-06-10 16:52:32 +0700

    Initial Commit

---
+++ RickMortyCharacters/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickMortyCharacters
+//
+//  Created by MAC on 6/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
